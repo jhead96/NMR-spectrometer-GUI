3ad10db7065e2088af2d1117984e5658097ab195
Jake Head <Jakehead96@gmail.com>
2022-08-25 15:20:40 +0100

    Added single pulse testing data.

--- GUI_pyQt/FPGA Testing/Power Levels/single_pulse_testing.py
+++ GUI_pyQt/FPGA Testing/Power Levels/single_pulse_testing.py
@@ -1,53 +1,34 @@
-import ADQ_tools_lite
+from ADQ_tools_lite import sdr14
 import numpy as np
 import time
 import sys
 
 # Connect to SDR-14
 try:
-    device = ADQ_tools_lite.sdr14()
+    device = sdr14()
 except Exception as ex:
     print("No device connected!")
     sys.exit()
-
 # Define pulse widths [ns]
-pulses = np.arange(10, 110, 10, dtype=int) * 1e3
+pulses = np.array([10, 20, 30, 40, 50, 100, 150, 200, 250, 500, 750, 1000]) * 1000
 # Define reg
 reg = 2
 
 for p in pulses:
 
     # Write
-    if device.reg_write(reg, p):
-        print(f"Write successful")
-    else:
+    if not device.reg_write(reg, p):
         print("Write unsuccessful, exiting ...")
         break
 
-    # Read
-    if device.reg_read(reg) == p:
-        print(f"Reg value = {p} ns")
-    else:
-        print("Read failed, exiting ...")
-        break
-
-    time.sleep(1)
+    time.sleep(0.5)
     # Enable/disable
     device.enable_dev()
     print("Device enabled!")
-    time.sleep(5)
+    time.sleep(8)
     device.disable_dev()
     print("Device disabled!")
-    time.sleep(1)
+    time.sleep(0.5)
     print("--------")
 
-
-
-
-
-
-
-
-
-
-
+print("Finished!")
