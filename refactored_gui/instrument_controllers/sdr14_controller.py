ef1701f174a64049c94ab2e42557249cae32ee6e
Jake Head <Jakehead96@gmail.com>
2022-11-25 12:09:07 +0000

    Renamed directories.

--- refactored_gui/instrument_controllers/sdr14_controller.py
+++ refactored_gui/instrument_controllers/sdr14_controller.py
@@ -5,7 +5,7 @@ from ctypes.util import find_library
 
 class SDR14():
 
-    def __init__(self, api_path: str = "\\GUI_pyQt\\Refactored_gui\\instrument_controllers\\ADQAPI.dll") -> None:
+    def __init__(self, api_path: str = "\\GUI_pyQt\\refactored_gui\\instrument_controllers\\ADQAPI.dll") -> None:
         self.api_path = api_path
         self.api = self.retrieve_api()
 
@@ -26,6 +26,6 @@ class SDR14():
 
 
 x = SDR14()
-# print(find_library(r"\\adf.bham.ac.uk\eps\prhome21\J\JXH1264\Research\FPGA Development\NMR-spectrometer-GUI\NMR-spectrometer-GUI\GUI_pyQt\Refactored_gui\instrument_controllers\ADQAPI.dll"))
+# print(find_library(r"\\adf.bham.ac.uk\eps\prhome21\J\JXH1264\Research\FPGA Development\NMR-spectrometer-GUI\NMR-spectrometer-GUI\GUI_pyQt\refactored_gui\instrument_controllers\ADQAPI.dll"))
 
 
